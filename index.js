6f4713d56c58f1d19f77343384c50237ab9cfec1
Abid Hasan <ab1dhasan@outlook.com>
2022-11-24 13:57:00 +0600

    added a minimal server

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+
+const port = process.send.PORT || 5000;
+
+// initialize express
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+	req.send('Resale market is running...');
+});
+
+app.listen(port, () => console.log('Listening to port', port));
